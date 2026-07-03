4331d3878e813e2f1503dac27fb63b15b6688c59
Ania-su <antsaaa.randria@gmail.com>
2026-07-03 09:04:49 +0300

    feat: not found, bad request, mismatch type error

---
+++ src/main/java/hei/school/subscribe/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package hei.school.subscribe.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
