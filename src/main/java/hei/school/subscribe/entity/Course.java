ef353987934ff705c692211b5754f153e45cdcd3
Rindraniaina Rasolofondrasoa <rindraniainarasolofondrasoa17@gmail.com>
2026-07-03 08:49:20 +0300

    feat: entity

---
+++ src/main/java/hei/school/subscribe/entity/Course.java
@@ -0,0 +1,20 @@
+package hei.school.subscribe.entity;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Course {
+  private UUID id;
+  private String title;
+  private Instant startDate;
+  private Instant instant;
+}
---
+++ src/main/java/hei/school/subscribe/entity/User.java
@@ -0,0 +1,20 @@
+package hei.school.subscribe.entity;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class User {
+  private UUID id;
+  private String firstName;
+  private String lastName;
+  private String userName;
+  private String email;
+}
